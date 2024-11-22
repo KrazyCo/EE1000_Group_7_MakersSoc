5925f74d48d0f6a96582e018248d83b11cd12a6e
KrazyCo <conorstrickland@hotmail.com>
2024-11-22 16:58:05 +0000

    first session done - servo, led strip, ultrasonic sensor, ldr working - speaker needs opamp and then can work on software

--- EE1000_Group_7_makersSoc.ino
+++ EE1000_Group_7_makersSoc.ino
@@ -1,14 +1,150 @@
+#include <Servo.h>
+#include <FastLED.h>
+
+// *pin d3: 360 degree servo data
+// *pin d9: led strip data
+// *pin d12: ultrasonic trig line
+// *pin d13: ultrasonic echo line
+
+// *pin a0: LDR line
+// !pin a1: speaker line
+
+// todo: see if can set ldr pin to a uint8_t to have it in a variable pointer
+// todo: fix startDistance to change on startup as couldnt figure out the bug where startDistance gets set to 128.00 after first activation
+
+// setup servo
+const int servoPin = 3;
+Servo servo;
+
+// setup ultrasonic
+const int trigPin = 12;
+const int echoPin = 13;
+double duration, distance;
+const float startDistance = 32;
+
+// setup LED strip
+#define LED_PIN 9
+#define NUM_LEDS 30
+CRGB leds[NUM_LEDS];
+int i{};
+int brightness{50};
 
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
+void setup()
+{
+    Serial.begin(115200);
+
+    // setup servo
+    servo.attach(servoPin);
+
+    // setup ultrasonic
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+
+    // setup LDR
+    pinMode(A0, INPUT);
+
+    // setup LED strip
+    FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+    FastLED.setBrightness(brightness);
 }
 
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  Serial.println("HELLO VSCODE!!!");
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+void loop()
+{
+    //servo.write(180);
+    Serial.println("servo forwards");
+    Serial.println(measureUltrasonic());
+    Serial.println(measureLDR());
+    Serial.println(startDistance);
+    Serial.println(isObjectClose());
+    ultrasonicLoop();
+    delay(1000);
+    //servo.write(0);
+    Serial.println("servo stop");
+    Serial.println(measureUltrasonic());
+    Serial.println(measureLDR());
+    Serial.println(startDistance);
+    Serial.println(isObjectClose());
+    ultrasonicLoop();
+    delay(1000);
+}
+
+void ultrasonicLoop()
+{
+    if (isObjectClose()) {
+        LEDTestPattern();
+    } else {
+        clearLEDStrip();
+    }
+}
+
+// returns a float of what the ultrasonic sensor is currently sensing
+float measureUltrasonic()
+{
+    // create a pulse
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+
+    digitalWrite(trigPin, HIGH); // Create a 10 microsecond pulse
+    delayMicroseconds(10);
+
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+
+    distance = (((duration * 1000.0) / 340.0) / 100.0) / 2.0; // calculate distance in CM
+    return distance;
+}
+
+// returns the value (0-1023) of the LDR
+int measureLDR()
+{
+    return analogRead(A0);
+}
+
+// sets the LED strip to the test pattern
+void LEDTestPattern()
+{
+    for (int i = 0; i <= NUM_LEDS; i++)
+    {
+        if (i < 5)
+        {
+            leds[i] = CRGB(0, 0, 255);
+        }
+        else if (i < 10)
+        {
+            leds[i] = CRGB(255, 0, 255);
+        }
+        else if (i < 15)
+        {
+            leds[i] = CRGB(255, 0, 0);
+        }
+        else if (i < 20)
+        {
+            leds[i] = CRGB(255, 255, 0);
+        }
+        else if (i < 25)
+        {
+            leds[i] = CRGB(0, 255, 0);
+        }
+        else if (i <= 30)
+        {
+            leds[i] = CRGB(255, 255, 255);
+        }
+        FastLED.show();
+    }
+}
+
+// sets the LED strip to all off
+void clearLEDStrip()
+{
+    for (int i = 0; i <= NUM_LEDS; i++)
+        leds[i] = CRGB(0, 0, 0);
+    FastLED.show();
+}
+
+// returns true if the ultrasonic sensor is currently at less than half of the
+// distance when the arduino initilised
+bool isObjectClose()
+{
+    return (measureUltrasonic() < (startDistance / 1.1));
 }
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 "# EE1000_Group_7_MakersSoc" 
+
+pin d3: 360 degree servo data
+pin d9: led strip data
+pin d12: ultrasonic trig line
+pin d13: ultrasonic echo line
+
+pin a0: speaker line
+pin a1: LDR line 
